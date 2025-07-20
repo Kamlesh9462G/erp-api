d401b882be52b53a8119d95e08423dffe1f59a63
Kamlesh <kamleshgurjar950@gmail.com>
2025-07-20 22:39:11 +0530

    updated

--- apps/company-api/src/api/routes/role.route.js
+++ apps/company-api/src/api/routes/role.route.js
@@ -4,14 +4,25 @@ const checkPermission = require("@erp-system/common/middlewares/permission");
 const { roleController } = require("../controllers/index");
 const router = express.Router();
 
-router.post("/", 
-    // auth, checkPermission("add_role"),
-     roleController.addRole);
-router.get("/",
-     auth, checkPermission("get_roles"),
-      roleController.getRoles);
-router.get("/:role_id", auth, checkPermission("get_role"), roleController.getRole);
-router.patch("/:role_id", auth, checkPermission("update_role"), roleController.updateRole);
-router.delete("/:role_id", auth, checkPermission("delete_role"), roleController.deleteRole);
+router.post("/", auth, checkPermission("add_role"), roleController.addRole);
+router.get("/", auth, checkPermission("get_roles"), roleController.getRoles);
+router.get(
+  "/:role_id",
+  auth,
+  checkPermission("get_role"),
+  roleController.getRole
+);
+router.patch(
+  "/:role_id",
+  auth,
+  checkPermission("update_role"),
+  roleController.updateRole
+);
+router.delete(
+  "/:role_id",
+  auth,
+  checkPermission("delete_role"),
+  roleController.deleteRole
+);
 
 module.exports = router;
